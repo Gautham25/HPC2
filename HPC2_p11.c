be7f69744f2605f78b34027747f94eafc89b90c7
Gautham25 <32765396+Gautham25@users.noreply.github.com>
2017-10-27 17:13:04 -0700

    Update HPC2_p11.c

--- HPC2_p11.c
+++ HPC2_p11.c
@@ -56,7 +56,7 @@ int main()
 {
     srand((double)time(NULL));
     int ubound = 100, lbound = 1;
-    int arrayLen[] = {10};//,2000,3000,4000,5000};
+    int arrayLen[] = {1000};//,2000,3000,4000,5000};
     int size = (sizeof(arrayLen)/sizeof(arrayLen[0]));
     int n,j,i;
     printf("Using LAPACK Library\n");
@@ -98,44 +98,45 @@ int main()
         // correct solution = {0 2 -1}'
         // LU factorization
 
-        // char     SIDE = 'L';
-        // char     UPLO = 'L';
-        // char     DIAG = 'U';
-        // int      M    = 1;
-        // double   a    = 1.0;
-        // clock_gettime(CLOCK_MONOTONIC,&tstart);
-        // LAPACK_dgetrf(&N,&N,arrA,&LDA,IPIV,&INFO);
-        // clock_gettime(CLOCK_MONOTONIC,&tend);
-        // double time = ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) - ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec);
-        // // This function solve the Ax=B directly
-        // //dgetrs_(&TRANS,&N,&NRHS,A,&LDA,IPIV,B,&LDB,&INFO);
-        //
-        // // change the order of B according to IPIV[] from LU factorization
-        //
-        // for(i = 0; i < N; i++)
+        char     SIDE = 'L';
+        char     UPLO = 'L';
+        char     DIAG = 'U';
+        int      M    = 1;
+        double   a    = 1.0;
+        clock_gettime(CLOCK_MONOTONIC,&tstart);
+        LAPACK_dgetrf(&N,&N,arrA,&LDA,IPIV,&INFO);
+        clock_gettime(CLOCK_MONOTONIC,&tend);
+        double time = ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) - ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec);
+        // This function solve the Ax=B directly
+        //dgetrs_(&TRANS,&N,&NRHS,A,&LDA,IPIV,B,&LDB,&INFO);
+
+        // change the order of B according to IPIV[] from LU factorization
+
+        for(i = 0; i < N; i++)
+        {
+            double tmp = arrB[IPIV[i]-1];
+        	arrB[IPIV[i]-1] = arrB[i];
+        	arrB[i] = tmp;
+        }
+
+        // forward  L(Ux) = B => y = Ux
+        dtrsm_(&SIDE,&UPLO,&TRANS,&DIAG,&N,&M,&a,arrA, &N, arrB, &N);
+        UPLO = 'U';
+        DIAG = 'N';
+        // backward Ux = y
+        dtrsm_(&SIDE,&UPLO,&TRANS,&DIAG,&N,&M,&a,arrA, &N, arrB, &N);
+
+        // printf("print the result : {\n");
+        // for (i=0;i<N;i++)
         // {
-        //     double tmp = arrB[IPIV[i]-1];
-        // 	arrB[IPIV[i]-1] = arrB[i];
-        // 	arrB[i] = tmp;
+    	//        printf("%f ",arrB[i]);
         // }
-        //
-        // // forward  L(Ux) = B => y = Ux
-        // dtrsm_(&SIDE,&UPLO,&TRANS,&DIAG,&N,&M,&a,arrA, &N, arrB, &N);
-        // UPLO = 'U';
-        // DIAG = 'N';
-        // // backward Ux = y
-        // dtrsm_(&SIDE,&UPLO,&TRANS,&DIAG,&N,&M,&a,arrA, &N, arrB, &N);
-        //
-        // // printf("print the result : {\n");
-        // // for (i=0;i<N;i++)
-        // // {
-    	// //        printf("%f ",arrB[i]);
-        // // }
-        // // printf("}\n");
-        // printf("Size N = %d\n",arrayLen[i]);
-        // printf("Time Taken = %.5f seconds\n",time);
-        // double gflops = (2*pow(n,3))/(3*time*pow(10,9));
-        // printf("\nPerformance in GFLOPS = %f\n",gflops);
+        printf("\n");
+        printArray(arrB);
+        printf("Size N = %d\n",arrayLen[i]);
+        printf("Time Taken = %.5f seconds\n",time);
+        double gflops = (2*pow(n,3))/(3*time*pow(10,9));
+        printf("\nPerformance in GFLOPS = %f\n",gflops);
 
     }
     return 0;
