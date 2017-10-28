85f9f74ee2458b91f5decdf6558913311518a70e
Gautham25 <32765396+Gautham25@users.noreply.github.com>
2017-10-27 23:58:02 -0700

    Update HPC2_p1.c

--- HPC2_p1.c
+++ HPC2_p1.c
@@ -241,6 +241,10 @@ int main()
         gflops = (2*pow(n,3))/(3*time*pow(10,9));
         printf("Time Taken = %.5f seconds\n",time);
         printf("\nPerformance in GFLOPS = %f\n",gflops);
+        printArray(arrA,n,1);
+        printf("\n");
+        printArray(arrA1,n,1);
+        printf("\n");
         printArray(arrB,n,1);
         printf("\n");
         printArray(x,n,1);
