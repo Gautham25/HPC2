df8a52c63240e104d232b13788a5e06596e7ffce
Gautham25 <32765396+Gautham25@users.noreply.github.com>
2017-10-27 23:33:01 -0700

    Update HPC2_p2.c

--- HPC2_p2.c
+++ HPC2_p2.c
@@ -248,6 +248,7 @@ void mydtrsm(int n, double *arrA, double *arrB, int *pvt, double *x, double *y, 
                 sum+=y[k]*arrA[i*n+k];
             }
             y[i] = arrB[pvt[i]]-sum;
+            sum = 0.0;
         }
     }
     else{
@@ -258,7 +259,7 @@ void mydtrsm(int n, double *arrA, double *arrB, int *pvt, double *x, double *y, 
             }
             temp = y[i]-sum;
             x[i] = temp/arrA[i*n+i];
-
+            sum = 0.0;
         }
     }
 }
