3273d831d1fb99f93222985059f9e4f75073dcdc
Gautham25 <32765396+Gautham25@users.noreply.github.com>
2017-10-29 16:50:45 -0700

    Update HPC2_p2.c

--- HPC2_p2.c
+++ HPC2_p2.c
@@ -186,7 +186,7 @@ int main(){
     srand((double)time(NULL));
     int *pvt,n,i,k,m,z;
     int ubound = 100, lbound = 0;
-    int arrN[] = {1000,2000,3000,4000,5000};
+    int arrN[] = {10};//,2000,3000,4000,5000};
     int block[] = {2};
     double random = randomNumber(ubound,lbound);
     double time,gflops;
